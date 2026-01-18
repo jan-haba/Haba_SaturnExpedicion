4c148f924e00d476eda7fad0480e3f75e6b9a709
JanHaba <janhabos404@gmail.com>
2026-01-18 15:55:22 +0100

    added 1 method to UI

--- src/Logic/UI.java
+++ src/Logic/UI.java
@@ -6,10 +6,11 @@ import Objects.Room;
 public class UI {
     private String lastMessage;
 
-    public void printWelcome() {}
-    public void printRoomDescription(Room room) {}
-    public void printInventory(Player player) {}
-    public void printTime(int time) {}
-    public void printHelp() {}
-    public void printLoopMessage() {}
+    public String printWelcome() {return null;}
+    public String printRoomDescription(Room room) {return null;}
+    public String printInventory(Player player) {return null;}
+    public String printTime(int time) {return null;}
+    public String printHelp() {return null;}
+    public String printClue(){return null;};
+    public String printLoopMessage() {return null;}
 }
